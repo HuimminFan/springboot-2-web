345d3a0292689a74e7b055fdab41cf1241e8165e
tom_proglb <hello_glb@fhm.com>
2018-07-12 23:10:43 +0800

    vv

---
+++ src/main/java/com/fhm/springboot/component/LoginHandlerInterceptor.java
@@ -0,0 +1,36 @@
+package com.fhm.springboot.component;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginHandlerInterceptor implements HandlerInterceptor{
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object user = request.getSession().getAttribute("loginUser");
+        if(user == null){
+            //没登录，回登录页
+            request.setAttribute("msg","权限不够请先登录");
+            request.getRequestDispatcher("/").forward(request,response);
+            return false;
+        }
+        else {
+            //登录了回当前页
+            return true;
+        }
+
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+
+    }
+}
--- src/main/java/com/fhm/springboot/controller/LoginController.java
+++ src/main/java/com/fhm/springboot/controller/LoginController.java
@@ -3,10 +3,9 @@ package com.fhm.springboot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 /**
@@ -18,10 +17,11 @@ public class LoginController {
 //    @RequestMapping(value = "/user/login",method = RequestMethod.POST)
     @PostMapping("/user/login")
     public String login(@RequestParam("username") String username,
-                        @RequestParam("password") String password, Map<String,Object> map){
-
-        if(!StringUtils.isEmpty(username) && "111111".equals(password))
-            return "dashboard";
+                        @RequestParam("password") String password, Map<String,Object> map, HttpSession session){
+        if(!StringUtils.isEmpty(username) && "111111".equals(password)) {
+            session.setAttribute("loginUser",username);
+            return "redirect:/main.html";
+        }
         else {
             map.put("msg","用户名或密码错误");
             return "login";
